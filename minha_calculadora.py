230e0677117647c5ac3181d92207546d9d245497
Vinicius de Jesus Mendes de Paula <vinicius.mjesus@aluno.impacta.edu.br>
2026-03-27 11:06:02 -0300

    Inclusao da funcao de subracao

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -2,9 +2,13 @@
 def soma(a, b):
     return a + b
 
+def subracao(a, b):
+    return a - b
+
 def main():
     x = int(input('x? '))
     y = int(input('y? '))
     print(f'Soma: = {soma(x, y)}')
+    print(f'Subracaso: = {subracao(x, y)}')
 
 main()
