a3d36e2392d1b42dd99238e1d2f6ab6dc452979e
Vinicius de Jesus Mendes de Paula <vinicius.mjesus@aluno.impacta.edu.br>
2026-03-27 11:39:00 -0300

    funcao multiplica

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -1,14 +1,17 @@
-
 def soma(a, b):
     return a + b
 
 def subracao(a, b):
     return a - b
 
+def multiplica(a, b):
+    return a * b
+
 def main():
     x = int(input('x? '))
     y = int(input('y? '))
     print(f'Soma: = {soma(x, y)}')
     print(f'Subracaso: = {subracao(x, y)}')
+    print(f'Multiplicacao: = {multiplica(x, y)}')
 
 main()
